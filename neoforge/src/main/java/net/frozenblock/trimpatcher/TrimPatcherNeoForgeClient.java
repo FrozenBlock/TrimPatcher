2a1ba879bb2ea1a377e7d71d92184c329cd089cf
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2026-03-28 14:10:54 +1100

    unstable logging

--- neoforge/src/main/java/net/frozenblock/trimpatcher/TrimPatcherNeoForgeClient.java
+++ neoforge/src/main/java/net/frozenblock/trimpatcher/TrimPatcherNeoForgeClient.java
@@ -1,15 +1,15 @@
 package net.frozenblock.trimpatcher;
 
-import com.google.common.collect.ImmutableMap;
-import net.minecraft.client.resources.model.sprite.TextureSlots;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(value = TPConstants.MOD_ID, dist = Dist.CLIENT)
 public class TrimPatcherNeoForgeClient {
 
     public TrimPatcherNeoForgeClient(IEventBus modEventBus) {
+		TPConstants.UNSTABLE_LOGGING = !FMLLoader.getCurrent().isProduction();
         TrimPatcherClient.init();
     }
 }
